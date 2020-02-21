8e0d36cccec74bf70bc700f7f01f55850c5112de
ApthNZ <a.r.wilkinson@gmail.com>
2020-02-21 20:16:30 +1300

    Add task create and task read functions

--- task_muppet.py
+++ task_muppet.py
@@ -8,15 +8,42 @@ will do a web interface later
 """
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
 
+
+# Instructions
 @app.route('/')
 def index():
     """Index route function"""
-    return jsonify("Hello, World!")
+    return read_tasks()
 
+# Task Creator
+@app.route('/create')
+def create():
+    """Allow creation of task via query string"""
+    description = request.args.get('description')
+    status = 'Not Done'
+    write_task(description, status)
+    return "Task written to file\n"
+    # TODO: Handle args being nothing
+
+# File Manager
+def write_task(description, status):
+    """Append a new task to the tasks file"""
+    with open('tasks.txt', 'a+') as file:
+        task_json = jsonify({'description': description, 'status': status})
+        file.write(task_json.get_data(as_text=True))
+        # TODO: Put everything into one big JSON block, not separate blocks
+
+def read_tasks():
+    """Read all the tasks in the file"""
+    with open('tasks.txt', 'r') as file:
+        return ''.join(file.readlines())
+
+
+# Run the app
 if __name__ == '__main__':
     app.run()
